3b6547c74fcacf0e47fb09aae29f40c3222b1d66
mshipan <mshipan657@gmail.com>
2024-08-17 13:32:42 +0600

    Complete variable, data types and Operators

---
+++ JavaScript-Fundamental/Variables-DataTypes-and-Operators.js
@@ -0,0 +1,67 @@
+// There are three types of Variables
+// They are var, let and const
+
+// ****** variable declaration ******
+// var
+var name = "Shipan Mallik";
+var my_name = "Shipan Mallik";
+var myName = "Shipan Mallik";
+// these types of varialbes are valid
+
+// let
+let persom = "Shipan";
+
+// const
+const someOne = "Shipan Mallik";
+
+// "var" is global scope, "let" is global and block both scope and "const" is only block scope
+
+// ****** Data Types ******
+
+// There are two types of data.
+// 1. Primitive Data Types
+// 1. Non-Primitive Data Types
+
+// Primitive Data Types:
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+
+// Non-Primitive Data Types:
+// 1. Array
+// 2. Object
+// 3. Function
+
+// ****** Operator ******
+
+// 1. Arithmetic Operator
+// +, -, *, /, % (modulus), ++ (increment), -- (decrement)
+
+// 2. Comparision Operation
+// ==, !=, >, <, >=, <=
+
+// N.B:
+// = (use to assign a variable)
+// == (use to compare values between two variables)
+// === (use to compare both values and data types between two variables)
+
+// 3. Logical Operator
+// &&, ||, !
+
+// 4. assignment operator
+// =, +=, -=, *=, /=, %=
+
+// 5. Conditional / Ternary Operator
+// ..?.. : ..
+var a = 10;
+var b = 20;
+
+var c = a > b ? 100 : 200;
+console.log(c);
+
+// Other Operator
+// typeof
+
+console.log(typeof c);
