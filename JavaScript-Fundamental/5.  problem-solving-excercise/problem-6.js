93baa80ef593828f5c161124279465044e647554
mshipan <mshipan657@gmail.com>
2024-08-22 20:00:26 +0600

    problem 6 solved

---
+++ JavaScript-Fundamental/5.  problem-solving-excercise/problem-6.js
@@ -0,0 +1,11 @@
+// Write a program that determines whether a given number is positive, negative, or zero, and outputs an appropriate message.
+
+let num = -4;
+
+if (num > 0) {
+  console.log("This is a positive number");
+} else if (num < 0) {
+  console.log("This is a negative number");
+} else if (num === 0) {
+  console.log("This is zero");
+}
