88df80ef6f63ef824bccacfdf7f5bb11c4b23b28
mshipan <mshipan657@gmail.com>
2024-08-30 22:59:52 +0600

    problem 7 solved

---
+++ es6+/7. problem-solving-exercise/problem-7.js
@@ -0,0 +1,14 @@
+// Create a function that merges two arrays of numbers into a single sorted array using the spread operator.
+const mergeArrays = function (arr1, arr2) {
+  const mergedArr = [...arr1, ...arr2];
+  console.log(mergedArr);
+  const sortedArr = mergedArr.sort((a, b) => a - b);
+
+  return sortedArr;
+};
+
+const array1 = [2, 6, 3, 1, 5];
+const array2 = [10, 8, 11, 9, 7];
+
+const result = mergeArrays(array1, array2);
+console.log(result);
